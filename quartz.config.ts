4eba6a573760e211b82a50715dd005ed23e13839
Luciana <lucianatanza@gmail.com>
2025-02-13 23:28:02 +0100

    vault backup: 2025-02-13 23:28:02

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "lux.github.io",
+    baseUrl: "lucianatanzarella.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "published",
     generateSocialImages: true,
