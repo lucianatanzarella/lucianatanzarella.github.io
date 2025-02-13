2ce823e64b2303f9ef80f4b1f19c0bcea4b1b8ce
Luciana <lucianatanza@gmail.com>
2025-02-14 00:17:22 +0100

    vault backup: 2025-02-14 00:17:22

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Deed I Do",
+    pageTitle: "Tourmaline",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
