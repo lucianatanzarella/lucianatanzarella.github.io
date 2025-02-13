a005fd8d80564b714a3d69de01d3ec7ab1950724
Luciana <lucianatanza@gmail.com>
2025-02-13 22:11:23 +0100

    vault backup: 2025-02-13 22:11:23

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,10 +30,10 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
+          light: "#F0F0D7",
+          lightgray: "#D0DDD0",
+          gray: "#AAB99A",
+          darkgray: "#727D73",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
